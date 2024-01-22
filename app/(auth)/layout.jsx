0308938e9a2134e41d31e3cda65279ee8865a8dc
arjundangi01 <arjundangi907473@gmail.com>
2024-01-22 09:22:54 +0530

    done all

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const AuthLayout = ({children}) => {
+const AuthLayout = ({ children }) => {
   return (
-      <div className='flex items-center justify-center h-[100vh] ' >{children}</div>
-  )
-}
+    <div className="flex items-center justify-center h-[100vh] ">
+      {children}
+    </div>
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
